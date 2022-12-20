bd62a518f486908dfec0b47eadcfb2265d0324a3
Ivan <markelovv1999@yandex.ru>
2022-12-20 19:23:57 +0300

    create new fold for new hw

---
+++ oop/sem1/HotDrink.java
@@ -0,0 +1,41 @@
+package oop.sem1;
+
+public class HotDrink {
+    protected String name;
+    protected String type;
+    protected int volume;
+    protected int temperature;
+
+    public HotDrink(String name, String type, int volume, int temperature) {
+        this.name = name;
+        this.type = type;
+        this.volume = volume;
+        this.temperature = temperature;
+    }
+
+    public String getType (String type) {
+        return type;
+    }
+
+    public void setType (String type) {
+        this.type = type;
+    }
+
+    public int getTemperature(int temperature) {
+        return temperature;
+    }
+
+    public void setTemperature(int temperature) {
+        this.temperature = temperature;
+    }
+
+    public int getVolume(int volume) {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+
+}
